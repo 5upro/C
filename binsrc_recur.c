bfd2f185432b4dd7a346023f0281ffd9cc03b6f2
SUPRØ <00youareworngtho00@gmail.com>
2024-07-19 06:26:29 +0530

    062630_19072024

--- binsrc_recur.c
+++ binsrc_recur.c
@@ -1,16 +1,16 @@
 #include "stdio.h"
 
-int Rec_binsrc(int *arr, int high, int sval){
+int binsrc_recur(int *arr, int high, int sval){
     static int low = 0;
     int mid;
     if(low<=high){
         mid = (low+high)/2;
         if(arr[mid]<sval){
             low = mid+1;
-            return Rec_binsrc(arr, high, sval);
+            return binsrc_recur(arr, high, sval);
         }
         else if(arr[mid]>sval){
-            return Rec_binsrc(arr, mid-1, sval);
+            return binsrc_recur(arr, mid-1, sval);
         }
         else if(arr[mid]==sval){
             low = 0;
@@ -21,4 +21,21 @@ int Rec_binsrc(int *arr, int high, int sval){
         low = 0;
         return -1;
     }
+}
+int main(){
+    int arr[]={9, 13, 24, 25, 32, 39, 44, 49}, n=8;
+    
+    for(int i=0; i<n; i++){
+        printf("%d : %d\n", i, arr[i]);
+    }
+    
+    for(int i=0; i<18; i++){
+        printf("%d ", binsrc_recur(arr, n, arr[i]));
+    }printf("\n");
+
+    for(int i=0; i<18; i++){
+        printf("%d ", binsrc_recur(arr, n, arr[i]));
+    }printf("\n");
+    
+    return 0;
 }
